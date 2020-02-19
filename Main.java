0978b5ca28a9d0754f804f9a57baecddd8b1f633
Izzyharvin <51219055+Izzyharvin@users.noreply.github.com>
2020-02-19 00:08:12 -0500

    Started basic learning of Java

---
+++ Main.java
@@ -0,0 +1,16 @@
+//class = a container for related functions
+//Every java program should have atleast have one class that contains the main function
+//When a function belongs to a class it is called a method
+//public = access
+public class Main {
+    // void = reserve keyword
+    // main = entry point of function
+    // parenthess = parameters
+    // curly braces = executed function
+    public static void main(String[] args) {
+        //String is like a variable that is equal to whatever you put
+        String x = "Test";
+        //Will print out whats in the parenthess
+        System.out.println(x);
+    }
+}
